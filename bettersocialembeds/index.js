e69484d095275d59fd5650879c2ab66618eb9262
leftwithshenron <p06666400@gmail.com>
2026-05-25 16:46:42 -0300

    Update index.js

--- bettersocialembeds/index.js
+++ bettersocialembeds/index.js
@@ -18,28 +18,26 @@
             req?.body?.content &&
             typeof req.body.content === "string"
           ) {
-            try {
-              let c = req.body.content;
+            let c = req.body.content;
 
-              c = c.replace(
-                /https?:\/\/(?:www\.)?(?:twitter|x)\.com([^\s]+)/gi,
-                "https://fixupx.com$1"
-              );
+            c = c.replace(
+              /https?:\/\/(?:www\.)?(?:twitter|x)\.com([^\s]+)/gi,
+              "https://fixupx.com$1"
+            );
 
-              c = c.replace(
-                /https?:\/\/(?:www\.)?instagram\.com([^\s]+)/gi,
-                "https://www.vxinstagram.com$1"
-              );
+            c = c.replace(
+              /https?:\/\/(?:www\.)?instagram\.com([^\s]+)/gi,
+              "https://www.vxinstagram.com$1"
+            );
 
-              c = c.replace(
-                /https?:\/\/(?:[a-z0-9]+\.)?tiktok\.com([^\s]+)/gi,
-                "https://tnktok.com$1"
-              );
+            c = c.replace(
+              /https?:\/\/(?:[a-z0-9]+\.)?tiktok\.com([^\s]+)/gi,
+              "https://tnktok.com$1"
+            );
 
-              req.body.content = c;
-            } catch (e) {
-              console.error("[BetterSocialEmbeds] Regex error:", e);
-            }
+            req.body.content = c;
+
+            return send(req);
           }
 
           return send(req);
